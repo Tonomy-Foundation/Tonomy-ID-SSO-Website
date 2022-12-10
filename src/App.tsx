866e824279385fc9e9dff10ffa21ed8874ddcf42
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-10 17:05:59 +0100

    added verification psuedocode

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Bytes, KeyType, PrivateKey } from '@greymass/eosio';
-import { App as APP } from 'tonomy-id-sdk';
+import { App as TonomyApp, JWTLoginPayload } from 'tonomy-id-sdk';
+
 function App() {
+    async function sendRequestToMobile(requests: JWTLoginPayload[], channel) {
+        /*
+        if (loggedIn) {
+            send a new request with
+            {
+                verifiedJwt
+            }
+        } else {
+            send a new request with
+            {
+                idTonomyJwt,
+                verifiedJwt
+            }
+        }
+        */
+    }
+
+    async function handleRequests() {
+        const verifiedJwt = await TonomyApp.onRedirectLogin();
+        /*
+        let idTonomyJwt: string;
+        const loggedIn = user logged into id.tonomy.foundation (check local storage and validate key is still authorized)
+        if (loggedIn) {
+            idTonomyJwt = get from local storage
+        } else {
+            idTonomyJwt = TonomyApp.onPressLogin();
+        }
+
+        if (mobile) {
+            sendRequestToMobile([idTonomyJwt, verifiedJwt], deeplink);
+        } else {
+            if (loggedIn) {
+                subscribe to the channel
+            } else {
+                create new channel by creating QR code with idTonomyJwt
+            }
+            sendRequestToMobile([idTonomyJwt, verifiedJwt], communication channel);
+        }
+        */
+    }
+
     useEffect(() => {
-        APP.onPressLogin(window).then((result) => console.log(result));
+        handleRequests();
     });
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
+                <p>id.tonomy.foundation (localhost:3000)</p>
             </header>
         </div>
     );

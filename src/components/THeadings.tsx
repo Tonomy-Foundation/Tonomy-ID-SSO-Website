36d7905ce493ebde11b5c5451f3e8a5923890782
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-12 19:16:52 +0100

    Added component types

--- src/components/THeadings.tsx
+++ src/components/THeadings.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function TH1(props: any) {
+export function TH1(props: React.HTMLAttributes<HTMLHeadingElement>) {
     return <h1 {...props} />;
 }
 
-export function TP(props: any) {
+export function TP(props: React.HTMLAttributes<HTMLParagraphElement>) {
     return <p {...props} />;
 }

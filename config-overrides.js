1129fd2eb6a25eb39b2bc3f1f995c6e9befb45bc
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-15 17:06:55 +0100

    Installed react-app-rewire-alias and mui

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,28 @@
 const webpack = require('webpack');
+const { aliasDangerous } = require('react-app-rewire-alias/lib/aliasDangerous');
+
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
     const fallback = config.resolve.fallback || {};
+
     Object.assign(fallback, {
         crypto: require.resolve('crypto-browserify'),
     });
+
     config.resolve.fallback = fallback;
+
+    // REACT_ APP_ NODE_ ENV is set in Tonomy-Integration/scripts/helpers.sh
+    if (process.env.REACT_APP_NODE_ENV === 'local') {
+        // https://github.com/risenforces/craco-alias/issues/24#issuecomment-801176626
+        console.log('Local environment detected, using local Tonomy-ID-SDK');
+
+        const aliasConfigModifier = aliasDangerous({
+            'tonomy-id-sdk': __dirname + '/../Tonomy-ID-SDK',
+        });
+
+        aliasConfigModifier(config);
+    }
+
     config.plugins = [
         ...config.plugins,
         new webpack.ProvidePlugin({
